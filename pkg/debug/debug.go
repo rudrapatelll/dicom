ed3f04bf38312cf52a3fe4a4ed777745d1d28989
rudrapatelll <rudrap2601@gmail.com>
2025-11-29 18:30:00 +0000

    feat: add 15 feature implementations

---
+++ pkg/debug/debug.go
@@ -0,0 +1,16 @@
+//go:build debug
+// +build debug
+
+package debug
+
+import "log"
+
+// Log only logs when the debug build flag is present.
+func Log(data string) {
+	log.Println(data)
+}
+
+// Logf only logs with a formatted string when the debug build flag is present.
+func Logf(format string, args ...any) {
+	log.Printf(format, args...)
+}
---
+++ pkg/debug/default.go
@@ -0,0 +1,10 @@
+//go:build !debug
+// +build !debug
+
+package debug
+
+// Log only logs when the debug build flag is present.
+func Log(data string) {}
+
+// Logf only logs with a formatted string when the debug build flag is present.
+func Logf(format string, args ...any) {}
